67852810ad2c4c05d4046bbe87d1c580e2322fef
MikeLo <mike.hermes@me.com>
2015-12-10 21:38:09 -0700

    Added the user, temperature, and humidity models

---
+++ api/controllers/HumidityController.js
@@ -0,0 +1,11 @@
+/**
+ * HumidityController
+ *
+ * @description :: Server-side logic for managing humidities
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
