8f28a0f73c5d33518374b52c26823c0870e1d29a
MikeLo <mike.hermes@me.com>
2015-12-12 12:42:23 -0700

    added the code for the front-end to get the datas from the backend and display with with react.

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,25 +6,25 @@ var App = React.createClass({
   render: function() {
     return (
       <div>
-        <nav className="navbar navbar-default" role="navigation">
+        <nav className="navbar navbar-custom navbar-fixed-top" role="navigation">
           <div className="container">
-              <div className="navbar-header">
-                 <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                   <span className="sr-only">Toggle navigation</span>
-                   <span className="icon-bar"></span>
-                   <span className="icon-bar"></span>
-                   <span className="icon-bar"></span>
-                </button>
-                <a className="navbar-brand" href="/">IoT Dashboard</a>
-              </div>
-              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul className="nav navbar-nav navbar-right">
-                  <li><Link to="home">Home</Link></li>
-                  <li><Link to="page">Sign in</Link></li>
-                  <li><Link to="about">About</Link></li>
-                </ul>
-              </div>
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="/">IoT Dashboard</a>
             </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav navbar-right">
+                <li><Link to="page"><span className="glyphicon glyphicon-user"></span> Page</Link></li>
+                <li><Link to="about"><span className="glyphicon glyphicon-log-in"></span> About</Link></li>
+                
+              </ul>
+            </div>
+          </div>
         </nav>
 
         <div className="container">
@@ -43,6 +43,32 @@ var person = {
 };
 
 
+
+    // `io` is available as a global.
+    // `io.socket` will connect automatically, but at this point in the DOM, it is not ready yet
+    // (think of $(document).ready() from jQuery)
+    // 
+    // Fortunately, this library provides an abstraction to avoid this issue.
+    // Requests you make before `io` is ready will be queued and replayed automatically when the socket connects.
+    // To disable this behavior or configure other things, you can set properties on `io.sails`.
+    // You have one cycle of the event loop to set `io.sails.autoConnect` to false before the auto-connection
+    // behavior starts.
+    var newTempData = {
+        "value": 35 ,
+        "smell":"face",
+    };
+
+    io.socket.post('/humidity', newTempData ,function serverResponded (humidity, JWR) {
+      console.log("data");
+      if(JWR.statusCode == 200){
+        console.log("post worked");
+        console.log(humidity);
+      }
+    });
+
+
+
+
 var Home = React.createClass({
   render: function() {
     return (
@@ -55,15 +81,46 @@ var Home = React.createClass({
 });
 
 var Page = React.createClass({
+  getInitialState: function(){
+    return {
+      humidityList: []
+    };
+  },
+  componentDidMount: function(){
+    var component = this;
+    io.socket.get('/humidity', function serverResponded (humidity, JWR) {
+      if(JWR.statusCode == 200){
+        console.log("Succssess");
+        humidity;
+        console.log(humidity);
+        if (component.isMounted()) {
+          component.setState({
+            humidityList: humidity
+          });
+        }
+      }
+    }).bind(component);
+  },
+
   render: function() {
     return (
       <div>
         <h1>Page1</h1>
-        <p>Demo another page here</p>
+        <ul>
+            {this.state.humidityList.map(function(result) {
+              return <li key={result.id}>
+                        <ul>
+                          <li>{result.value}</li>
+                          <li>{result.createdAt}</li>
+                        </ul>
+                      </li>;
+            })}           
+        </ul>
       </div>
     );
   }
 });
+
 
 var About = React.createClass({
   render: function() {
@@ -85,10 +142,10 @@ var routes = (
       <Router>
         <Route name="app" path="/" component={App}>
           <Route name="page" path="/page" component={Page} />
-          <Route name="page2" path="/about" component={About} />
+          <Route name="about" path="/about" component={About} />
           <Route path="*" component={Home}/>
         </Route>
       </Router>
 );
 
-ReactDOM.render(routes, document.body)
+ReactDOM.render(routes, document.getElementById('content'))
